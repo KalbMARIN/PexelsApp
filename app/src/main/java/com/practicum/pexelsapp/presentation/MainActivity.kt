2fc36c5b287e3c60674dec26627861a5baf86a04
artembelonozhko <ar.belonozhko@gmail.com>
2026-04-25 17:07:14 +0300

    Fix bugs

--- app/src/main/java/com/practicum/pexelsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/practicum/pexelsapp/presentation/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.practicum.pexelsapp.presentation
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -59,6 +61,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             PexelsAppTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
+                    val isDarkTheme = isSystemInDarkTheme()
+                    val view = LocalView.current
+                    if (!view.isInEditMode) {
+                        SideEffect {
+                            val window = (view.context as Activity).window
+                            val controller = WindowCompat.getInsetsController(window, view)
+
+                            controller.isAppearanceLightStatusBars = !isDarkTheme
+                        }
+                    }
 
                     val isDataReady by splashViewModel.isReady.collectAsState()
                     val navController = rememberNavController()
